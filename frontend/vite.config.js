1feb224b410ca1554ac517a0467b694302bb6933
RealRoiSolutions <rrsprods@gmail.com>
2026-01-21 14:07:02 +0100

    Change icon paths to absolute in vite.config.js
    Updated icon paths to be absolute URLs.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: 'pwa_icon_192.png',
+            src: '/pwa_icon_192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa_icon_512.png',
+            src: '/pwa_icon_512.png',
             sizes: '512x512',
             type: 'image/png'
           }
