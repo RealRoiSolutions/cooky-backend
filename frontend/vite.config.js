fad44bccac67eb7f8c195ea03fd63454556c0193
RealRoiSolutions <rrsprods@gmail.com>
2026-01-21 14:22:01 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-
 export default defineConfig({
   plugins: [
     react(),
@@ -21,13 +20,25 @@ export default defineConfig({
             src: '/pwa_icon_192.png',
             sizes: '192x192',
             type: 'image/jpeg',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: '/pwa_icon_192.png',
+            sizes: '192x192',
+            type: 'image/jpeg',
+            purpose: 'maskable'
           },
           {
             src: '/pwa_icon_512.png',
             sizes: '512x512',
             type: 'image/jpeg',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: '/pwa_icon_512.png',
+            sizes: '512x512',
+            type: 'image/jpeg',
+            purpose: 'maskable'
           }
         ]
       }
